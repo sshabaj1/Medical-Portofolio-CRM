55549064a31e894210f713a56ea5dadfc4928342
Senad Shabaje <senadshabaj73@gmail.comm>
2021-10-10 16:07:59 +0200

    landing page update

--- website/views.py
+++ website/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.views.generic import TemplateView
+from tickets.forms import TicketCreateForm
 
 class LandindPageView(TemplateView):
     template_name = 'index.html'
+    form_class = TicketCreateForm
+
+    def get_success_url(self):
+        return reverse('landind_page')
